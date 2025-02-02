05f4d3371370b2a1c97113c52f592b1876bcde04
Soungalo Salam <dembelesalam27@gmail.com>
2025-02-02 22:55:44 +0100

    Update MainContext.js

--- src/State/MainContext.js
+++ src/State/MainContext.js
@@ -1,60 +1,51 @@
 import React, { useState } from "react";
-import {Users,Repos,Followers} from '../DummyData/UsersData'
+import { Users, Repos, Followers } from '../DummyData/UsersData';
 export const MainContext = React.createContext();
-const MainProvider = ({children}) =>{
+
+const MainProvider = ({ children }) => {
     const [userData, setUserData] = useState();
-    const [reposData, setReposData] = useState(" ");
-    const [followersData, setFollowersData] = useState(" ");
-    const [loading,setLoading] = useState(true);
-    const [searchTerm,setSearchTerm]= useState("creativekamrul");
-    // if(userData && reposData && followersData){
-    //     setLoading(false)
-    // }
-   // ...
-const getData = async (url) => {
-    const rawData = await fetch(url);
-    const convertedData = await rawData.json();
-    return convertedData;
-}
+    const [reposData, setReposData] = useState([]);
+    const [followersData, setFollowersData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState("creativekamrul");
 
-const getUserData = async (userUrl) => {
-    const nUserData = await getData(userUrl);
-    setUserData(nUserData);
-}
+    const getData = async (url) => {
+        const rawData = await fetch(url);
+        const convertedData = await rawData.json();
+        return convertedData;
+    }
 
-const getReposData = async (reposUrl) => {
-    const nReposData = await getData(reposUrl);
-    setReposData(nReposData);
-    console.log(nReposData);
-}
+    const getUserData = async (userUrl) => {
+        const nUserData = await getData(userUrl);
+        setUserData(nUserData);
+    }
 
-    const getFollowersData = async (followersUrl) =>{
+    const getReposData = async (reposUrl) => {
+        const nReposData = await getData(reposUrl);
+        setReposData(nReposData);
+        console.log(nReposData);
+    }
+
+    const getFollowersData = async (followersUrl) => {
         const nFollowersData = await getData(followersUrl);
         setFollowersData(nFollowersData);
     }
 
-
-        
- 
-    
-    return(
-        <div>
-            <MainContext.Provider value={ 
-            {
+    return (
+        <MainContext.Provider value={{
             loading,
             userData,
-            reposData, 
+            reposData,
             followersData,
             searchTerm,
             setSearchTerm,
             getUserData,
             getReposData,
-            getFollowersData}
-
-        }  >
-                {children}
-            </MainContext.Provider>
-        </div>
+            getFollowersData
+        }}>
+            {children}
+        </MainContext.Provider>
     )
 }
+
 export default MainProvider;
