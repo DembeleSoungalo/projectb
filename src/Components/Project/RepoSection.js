6cce2ad4116ee5c0f13dd97fa21bee7bcf4619ed
Soungalo Salam <dembelesalam27@gmail.com>
2025-02-02 22:53:09 +0100

    Update RepoSection.js

--- src/Components/Project/RepoSection.js
+++ src/Components/Project/RepoSection.js
@@ -1,38 +1,42 @@
-import React, { useContext, useEffect } from 'react'
-import { MainContext } from '../../State/MainContext'
-import {Row,Col,Container, Image, Button, Spinner} from 'react-bootstrap';
-const RepoSection = () => {
-  const {reposData,getReposData,searchTerm} = useContext(MainContext);
-  useEffect(()=>{
-    getReposData("https://api.github.com/users/" + searchTerm + "/repos");
-  },[])
-  
-  if(reposData){
-    return (
-      <div className="reposContainer mt-4">
-           {
-            reposData.map((e)=>{
-              return(
-                <div key={e.id} >
-                <div className="reposCard" >
-                  <h2>{e.name}</h2>
-                  <Button className="btn btn-primary"  ><a href={e.html_url} target="_blank" className='nav-link'>Go to the Github Page</a></Button>
-                </div>
-                </div>
-              )
-            })
-          }
-      </div>
-    )
-  }else{
-    return(
-      <div>
-        <Spinner animation="grow" >
-         
-        </Spinner>
-      </div>
-    )
-  }
-}
+import React, { useContext, useEffect } from 'react';
+import { MainContext } from '../../State/MainContext';
+import { Row, Col, Container, Image, Button, Spinner } from 'react-bootstrap';
 
-export default RepoSection
+const RepoSection = () => {
+  const { reposData, getReposData, searchTerm } = useContext(MainContext);
+
+  useEffect(() => {
+    if (typeof getReposData === "function") {
+      getReposData("https://api.github.com/users/" + searchTerm + "/repos");
+    } else {
+      console.error("getReposData is not a function");
+    }
+  }, [getReposData, searchTerm]);
+
+  if (!reposData) {
+    return (
+      <div>
+        <Spinner animation="grow" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="reposContainer mt-4">
+      {reposData.map((e) => (
+        <div key={e.id}>
+          <div className="reposCard">
+            <h2>{e.name}</h2>
+            <Button className="btn btn-primary">
+              <a href={e.html_url} target="_blank" className='nav-link'>
+                Go to the GitHub Page
+              </a>
+            </Button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RepoSection;
